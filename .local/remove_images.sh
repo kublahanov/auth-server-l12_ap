b0cbc3785e80cdbbb80a564a327f48c01216263f
Roman Kondrashov <nr.kondrashov@gmail.com>
2026-01-30 22:42:53 +0300

    Вынес mailpit в отдельный образ.

--- .local/remove_images.sh
+++ .local/remove_images.sh
@@ -2,4 +2,4 @@
 
 # Скрипт для удаления Docker образов проекта
 
-docker rmi auth_api-php83:latest auth_api-caddy:latest auth_api-postgres:latest axllent/mailpit:latest
+docker rmi auth_api-php83:latest auth_api-caddy:latest auth_api-postgres:latest auth_api-mailpit:latest
